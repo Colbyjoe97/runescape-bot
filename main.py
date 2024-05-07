49e798e7d148bb622e2ef1782a03dea7bd7809c3
colby <dumonchelle123@gmail.com>
2024-05-06 23:46:09 -0400

    removed unused code, made camera setting a default action

--- main.py
+++ main.py
@@ -8,25 +8,6 @@ from windowCapture import WindowCapture
 from vision import findClickPositions
 
 
-# wincap = WindowCapture('RuneLite')
-
-# loop_time = time()
-# while(True):
-
-#     screenshot = wincap.get_screenshot()
-#     # cv.imshow('Computer Vision', screenshot)
-#     # print(screenshot.astype)
-
-#     findClickPositions('images/resources/ore/copper.jpg', screenshot, 0.6, 'rectangles')
-#     # cv.resizeWindow('Computer Vision', 940, 730)
-
-#     print(f'FPS {1 / (time() - loop_time)}')
-#     loop_time = time()
-
-
-#     if cv.waitKey(1) == ord('q'):
-#         cv.destroyAllWindows()
-#         break
 
 # Click in a random spot between given px values
 def randNum(min, max):
@@ -39,8 +20,8 @@ def setCamera():
     pyautogui.moveTo((camera[0]+randNum(-10, 10)), (camera[1]+randNum(-10, 10)), duration=0.5)
     pyautogui.click()
 
-# pyautogui.moveTo(463,249,duration=1)
-# # GAME SIZE = 900 x 700
+# GAME SIZE = 900 x 700
+setCamera()
 # run = True
 # while run:
 #     setCamera()
